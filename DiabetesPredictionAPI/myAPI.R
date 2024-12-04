5c1e7bf0d1d5f7831811ffd143fcd0b496d4c3ca
Amanda Baright <ambarigh@ncsu.edu>
2024-12-04 15:38:34 -0500

    pred and info endpoints

--- DiabetesPredictionAPI/myAPI.R
+++ DiabetesPredictionAPI/myAPI.R
@@ -28,4 +28,69 @@ head(full_data)
 
 rf_recipe <- recipe(Diabetes_binary ~ ., data = full_data)
 
+rf_full_model <- rand_forest(mtry = 3) |>
+  set_engine("ranger") |>
+  set_mode("classification")
+
+rf_full_wkf <- workflow() |>
+  add_model(rf_full_model) |>
+  add_recipe(rf_recipe)
+
+final_model <- fit(rf_full_wkf, data = full_data)
+
+# We are asked to have default values for any predictors used in our 'best' model, such that if the
+# predictor is numeric use the mean of the variable, and if categorical use the most prevalent class.
+
+default_values <- full_data |>
+  summarise(
+    HighBP = levels(HighBP)[which.max(table(HighBP))],
+    HighChol = levels(HighChol)[which.max(table(HighChol))],
+    HeartDiseaseorAttack = levels(HeartDiseaseorAttack)[which.max(table(HeartDiseaseorAttack))],
+    PhysActivity = levels(PhysActivity)[which.max(table(PhysActivity))],
+    BMI = mean(BMI)
+  )
+
+# Set up Pred endpoint
+#* Predict Diabetes
+#* @param HighBP High blood pressure, No/Yes, default: No
+#* @param HighChol High cholesterol, No/Yes, default: No
+#* @param HeartDiseaseorAttack History of coronary heart disease or myocardial infection, No/Yes, default: No
+#* @param PhysActivity Physical activity in the past 30 days, not including job, No/Yes, default: Yes
+#* @param BMI Body mass index, default: 28.4
+#* @post /pred
+function(HighBP = default_values$HighBP,
+         HighChol = default_values$HighChol,
+         HeartDiseaseorAttack = default_values$HeartDiseaseorAttack,
+         PhysActivity = default_values$PhysActivity,
+         BMI = default_values$BMI) {
+  
+  input_data <- tibble(
+    HighBP = as.factor(HighBP),
+    HighChol = as.factor(HighChol),
+    HeartDiseaseorAttack = as.factor(HeartDiseaseorAttack),
+    PhysActivity = as.factor(PhysActivity),
+    BMI = as.numeric(BMI)
+  )
+  
+  predict(final_model, new_data = input_data, type = "prob") |>
+    pull(.pred_Yes) |>
+    round(4)
+  
+}
+
+# Example calls for /pred endpoint:
+# http://127.0.0.1:8000/pred
+# http://127.0.0.1:8000/pred?HighBP=Yes&HighChol=No&HeartDiseaseorAttack=No&PhysActivity=No&BMI=28
+# http://127.0.0.1:8000/pred?BMI=35&HighBP=Yes&HighChol=Yes
+
+# Info endpoint
+#* Info about the API
+#* @get /info
+function() {
+  list(
+    name = "Amanda Baright",
+    github_page = "https://ambaright.github.io/ST558FinalProject/"
+  )
+}
+
 
